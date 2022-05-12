efaa7ae2689bb628c4b5793d9779bdf9caa24382
mbudak <promaxcoq@gmail.com>
2022-05-12 19:37:33 -0400

    addUp: done

--- addUp/solution.js
+++ addUp/solution.js
@@ -1,1 +1,14 @@
 // Your solution here
+// Sum all sequental numbers that given the target
+function greatsum(target) {
+    var ret = 0; // default return value
+    for (let step = 1; step <= target; step++) {
+        ret = ret + step;
+    }
+    return ret;
+}
+
+
+console.log('input 4 ', greatsum(4));
+console.log('input 10 ', greatsum(10));
+console.log('input 2001 ', greatsum(2001));
