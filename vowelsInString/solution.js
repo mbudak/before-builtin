d6b87861d100ef5eea4c74c8bc23bbc27012fc2b
mbudak <promaxcoq@gmail.com>
2022-05-13 15:27:11 -0400

    Done

--- vowelsInString/solution.js
+++ vowelsInString/solution.js
@@ -1,1 +1,15 @@
-// Your solution here
+function numberOfVowels(str) {
+    var vowels = "aeiou";
+    ret = 0;
+    for (var i = 0; i < str.length; i++){
+        if (vowels.indexOf(str[i]) >= 0) { 
+            ret = ret + 1;
+        }
+    }
+    return ret;
+}
+
+
+console.log('hello -> ', numberOfVowels('hello'));
+console.log('you are great! -> ', numberOfVowels('you are great!'));
+console.log('why? -> ', numberOfVowels('why?'));
