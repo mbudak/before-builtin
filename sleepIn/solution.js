c469d70a9921e242ea6654b7ad8c242cbaca455f
mbudak <promaxcoq@gmail.com>
2022-05-13 14:17:07 -0400

    Done

--- sleepIn/solution.js
+++ sleepIn/solution.js
@@ -1,1 +1,11 @@
-// Your solution here
+function sleepIn(weekday, vacation){
+    if (weekday == false || vacation == true) {
+        return true;
+    }
+    return false;
+}
+
+console.log(' -> ', sleepIn(false, false))
+console.log(' -> ', sleepIn(false, true))
+console.log(' -> ', sleepIn(true, false))
+console.log(' -> ', sleepIn(true, true))
