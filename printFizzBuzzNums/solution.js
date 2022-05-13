6a137e47707d84a6ecc33e50dce4b432b05b5561
mbudak <promaxcoq@gmail.com>
2022-05-13 13:53:03 -0400

    Done

--- printFizzBuzzNums/solution.js
+++ printFizzBuzzNums/solution.js
@@ -1,1 +1,18 @@
 // Your solution here
+function printNumbers(){
+    for (var i = 1; i <= 100; i ++ ){
+        if (i % 3 == false & i % 5 == false) { // both 3rd and 5th is true
+            console.log('Result: ', i, 'FizzBuzz');
+        } else if (i % 3 == false ) { // each 3rd
+            console.log('Result: ', i, "Fizz");
+        } else if (i % 5 == false ) { // each 5th
+            console.log('Result: ', i, 'Buzz');
+        } else { // rest of it
+            console.log('Result: ', i);
+        }
+        
+    }
+}
+
+
+printNumbers();
