0b31f7f3540bd8546b43754e63c55f6118ce0ca6
mbudak <promaxcoq@gmail.com>
2022-05-13 01:33:24 -0400

    reverseDigit: Done

--- reverseDigit/solution.js
+++ reverseDigit/solution.js
@@ -1,1 +1,13 @@
 // Your solution here
+function reverseIt(arg) {
+    var ret = '';
+    // reverse loop
+    for (var i = arg.length-1; i >= 0; i--) {
+        ret = ret + arg[i];
+    }
+    return ret;
+}
+
+console.log('1234', reverseIt('1234'));
+console.log('1201', reverseIt('1201'));
+console.log('4', reverseIt('4'));
