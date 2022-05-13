eee6e22e7a40ddbcf0ba1bdc3a447dfe5c1dfd06
mbudak <promaxcoq@gmail.com>
2022-05-12 20:15:57 -0400

    isInteger: done

--- isInteger/solution.js
+++ isInteger/solution.js
@@ -1,1 +1,8 @@
-// Your solution here
+function isInteger(arg) {    
+    return Number.isInteger(arg); // ???
+}
+
+
+console.log('input "7" : ', isInteger("7"));
+console.log('input 7 : ', isInteger(7));
+console.log('input "4.3" : ', isInteger(4.3));
