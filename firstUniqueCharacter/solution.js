767c0320bd1b41dcde61b12784932ac7722fab38
mbudak <promaxcoq@gmail.com>
2022-05-13 12:34:20 -0400

    findFirstUniqueCharacter: Done

--- firstUniqueCharacter/solution.js
+++ firstUniqueCharacter/solution.js
@@ -1,1 +1,15 @@
-// Your solution here
+
+function findFirstUniqueChar(str) {
+    for (var i = 0; i < str.length; i++)
+    {
+        if (str.indexOf(str[i]) === str.lastIndexOf(str[i])) {
+            return str[i];
+        }
+        
+    }
+    
+}
+
+console.log("the quick brown fox jumps over the calm kitten quietly", findFirstUniqueChar("the quick brown fox jumps over the calm kitten quietly"));
+console.log("this hat is the greatest!", findFirstUniqueChar("this hat is the greatest!"));
+console.log("what a wonderful day it has been!", findFirstUniqueChar("what a wonderful day it has been!"));   
