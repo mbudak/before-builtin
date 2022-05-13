2b82ca029f5b4e0dacd016409a0450992191f803
mbudak <promaxcoq@gmail.com>
2022-05-13 14:23:17 -0400

    Done

--- smilingMonkeys/solution.js
+++ smilingMonkeys/solution.js
@@ -1,1 +1,11 @@
-// Your solution here
+function monkeyTrouble(aSmile, bSmile) {
+    if (aSmile & bSmile || !aSmile & !bSmile) {
+        return true;
+    }
+    return false;
+}
+
+console.log('both Smile', monkeyTrouble(true, true));
+console.log('only a Smile', monkeyTrouble(true, false));
+console.log('only b Smile', monkeyTrouble(false, true));
+console.log('both not Smile', monkeyTrouble(false, false));
