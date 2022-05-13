11aeff949d9bb3ad562a62263911cd4b9f6f1523
mbudak <promaxcoq@gmail.com>
2022-05-12 22:11:53 -0400

    averageOfArray: done

--- averageOfArray/solution.js
+++ averageOfArray/solution.js
@@ -1,1 +1,18 @@
 // Your solution here
+
+function average(array){
+    var sumall = 0;
+    
+    for (let i = 0; i < array.length; i++)
+    {
+        sumall = sumall + array[i];
+    }
+    
+    return sumall / array.length;
+}
+
+
+
+console.log('[ 1, 4, 7 ] -> ', average([ 1, 4, 7 ]));
+console.log('[ 10, 5 ] -> ', average([ 10, 5 ]));
+console.log('[ 1.5, 3, 2.5, 1 ] -> ', average([ 1.5, 3, 2.5, 1 ]));
