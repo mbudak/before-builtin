54c961e8b9a50df3c439f79c027444bae62c6413
mbudak <promaxcoq@gmail.com>
2022-05-12 23:26:54 -0400

    checkArrayForDups: Done

--- checkArrayForDups/solution.js
+++ checkArrayForDups/solution.js
@@ -1,1 +1,20 @@
 // Your solution here
+function isDublicateInAnArray(arg) {
+    var foundCount = 0;
+    for (var i = 0; i < arg.length; i++) {                
+        for (var j = 0; j < arg.length; j++) {
+            if (arg[i] === arg[j]) {
+                // console.log('LAN', arg[i], arg[j])
+                foundCount = foundCount + 1;
+            }
+        }
+        
+    }
+    if (foundCount > arg.length) {
+        return true;
+    }
+    return false;
+}
+
+console.log('[1,2,3,1]', isDublicateInAnArray([1,2,3,1]));
+console.log('[1,2,3,4]', isDublicateInAnArray([1,2,3,4]));
