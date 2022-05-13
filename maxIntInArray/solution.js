866aa07d35b4399599e18d698ebe95881c518acb
mbudak <promaxcoq@gmail.com>
2022-05-13 02:30:32 -0400

    maxIntInArray: Done

--- maxIntInArray/solution.js
+++ maxIntInArray/solution.js
@@ -1,1 +1,15 @@
 // Your solution here
+function findMaxInt(arg) {
+    var temp = '0';
+    for (var i = 0; i < arg.length; i++){
+        if (parseInt(temp) < parseInt(arg[i])) {
+            temp = arg[i];
+        }
+    }
+
+    return temp;
+}
+
+console.log('[ 1, 2, 3 ]', findMaxInt([ 1, 2, 3 ]));
+console.log('[ 3, 6, 4, 5, 2, 1 ]', findMaxInt([ 3, 6, 4, 5, 2, 1 ]));
+console.log('[ 3, 3, 3 ]', findMaxInt([ 3, 3, 3 ]));
